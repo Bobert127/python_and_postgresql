f35e5e4acf2148fb6498af8846533af6bdd34b87
Robert <roberttrzaska@outlook.com>
2021-07-04 13:12:35 +0200

    create database and table1

--- python_and_postgresql/create_db.py
+++ python_and_postgresql/create_db.py
@@ -3,6 +3,7 @@ from sqlite3 import OperationalError
 from psycopg2 import connect
 from psycopg2.errors import DuplicateDatabase
 
+CREATE_DB = "CREATE DATABASE workshop;"
 
 create_users_table = """create table users (
     id serial PRIMARY KEY,
@@ -16,25 +17,50 @@ create_messages_table = """ create table messages (
     TEXT VARCHAR(255),
     creation_date timestamp DEAFULT CURRENT_TIMESTAMP
 )"""
+USER = "postgres"
+password = "coderslab"
+host = "localhost"
+
+
 
 try:
+    cnx = connect(user=USER, password=password, host=host)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    try:
+        cursor.execute(CREATE_DB)
+        print("Database created")
+    except DuplicateDatabase as z:
+        print("Database exists ", z)
+    cnx.close()
+except OperationalError as e:
+    print("Connection Error: ", z)
 
-    USER = "postgres"
-    password = "coderslab"
-    host = "localhost"
-    database = "workshop"
 
-    cnx = connect(database=database, user=USER, password=password, host=host)
+try:
+    cnx = connect(database="workshop", user=USER, password=password, host=host)
     cnx.autocommit = True
     cursor = cnx.cursor()
     try:
         cursor.execute(create_users_table)
-        cursor.execute(create_users_table)
+        print("Table users created")
+    except DuplicateDatabase as d:
+        print("Table exists ", d)
+    cnx.close()
+except OperationalError as d:
+    print("Connection Error: ", d)
+
+
+try:
+    cnx = connect(database="workshop", user=USER, password=password, host=host)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    try:
+        cursor.execute(create_messages_table)
         print("Table created")
     except DuplicateDatabase as d:
         print("Table exists ", d)
     cnx.close()
-except OperationalError as e:
-    print("Connection Error: ", e)
-
+except OperationalError as d:
+    print("Connection Error: ", d)
 
