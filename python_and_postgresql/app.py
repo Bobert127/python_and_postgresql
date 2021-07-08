ff0a66c5d890ff4c622a9e9da401ba52d0b352e6
Robert <roberttrzaska@outlook.com>
2021-07-08 09:28:08 +0200

    exam2cz1_1

--- python_and_postgresql/app.py
+++ python_and_postgresql/app.py
@@ -1,31 +1,50 @@
+from sqlite3 import OperationalError
+
 from flask import Flask, request
+from psycopg2 import connect
 
 app = Flask(__name__)
 
-FORM1 = """
-<form method='GET'>
-    <label for="name">liczba1:</label><br>
-    <input type = "float" id = "name" name  = "liczba1"><br>
-    <label for="name">dzialanie</label><br>
-    <input type = "list" id = "name" name = "dzialanie"><br>
-    <label for="name">liczba2:</label><br>
-    <input type = "float" id = "name" name  = "liczba2"><br>
+FORM = """
+<form method='POST'>
+    <label for="name">name:</label><br>
+    <input type = "string" name  = "name"><br>
+    <label for="name">description</label><br>
+    <input type = "string"  name = "description"><br>
+    <label for="name">price:</label><br>
+    <input type = "float" name  = "price"><br>
     <label><button type="submit"> Prześlij</button></label>
 </form>
  """
+
+
 #
-#
-@app.route("/liczby/")
-#
-def get_liczby():
+def get_return_date(cur, name, description, price):
+    sql = "insert into items(name, description, price) values(%s, %s, %s)"
+    values = (name, description, price)
+    cur.execute(sql, values)
+
+
+@app.route("/add_product/", methods=("POST", "GET"))
+def produkc_return():
     if request.method == "GET":
-        return FORM1
-#
-@app.route("/liczby/")
-def get_twojeliczby():
-    liczba1_return = request.args["liczba1"]
-    liczba2_return = request.args["liczba2"]
-    return liczba1_return "+" liczba2_return  + "!"
+        return FORM
+    else:
+        name = request.form.get("name")
+        description = request.form.get("description")
+        price = request.form.get("price")
+        if len(name) > 60 or not price or not description:
+            return "Invalid date !!!"
+
+        try:
+            con = connect(database="exam2", user="postgres", password="coderslab", host="localhost")
+            con.autocommit = True
+            cursor = con.cursor()
+            get_return_date(cursor, name, description, price)
+            con.close()
+            return "Product added!"
+        except OperationalError as e:
+            return e
 
 if __name__ == '__main__':
     app.run(debug=True)
